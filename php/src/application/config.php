50c2218a857a7c98c110dacdfef7c32358c6a979
pd520c <you@example.com>
2016-11-21 16:40:25 +0800

    update

--- php/src/application/config.php
+++ php/src/application/config.php
@@ -87,9 +87,9 @@ return [
     // URL普通方式参数 用于自动生成
     'url_common_param'       => false,
     // URL参数方式 0 按名称成对解析 1 按顺序解析
-    'url_param_type'         => 0,
+    'url_param_type'         => 1,
     // 是否开启路由
-    'url_route_on'           => true,
+    'url_route_on'           => false,
     // 路由使用完整匹配
     'route_complete_match'   => false,
     // 路由配置文件（支持配置多个）
